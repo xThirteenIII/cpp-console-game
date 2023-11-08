db37e95920956ea5af32e61892cb19417e3fc4ab
xThirteenIII <giuseppe.richetti@gmail.com>
2023-11-08 20:02:11 +0100

    Add function to resize terminal window, commented coz it doesn't work

--- renderer/NcursesAdapter.cpp
+++ renderer/NcursesAdapter.cpp
@@ -7,6 +7,11 @@ void NcursesAdapter::initialize(){
     noecho(); // Disable echoing for user input
     curs_set(FALSE); // Hide cursor
     keypad(stdscr, TRUE);  // Enable special keys
+                           //
+    // Resize the terminal window to your desired dimensions (e.g., 25 rows and 80 columns)
+    //int rows = 25; // Set the number of rows
+    //int cols = 80; // Set the number of columns
+    //resize_term(rows, cols); // Resize the terminal
 }
 
 void NcursesAdapter::finalize(){
